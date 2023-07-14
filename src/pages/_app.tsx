651578abc1eaf2b46f5b69e2aa199f79b0001dc6
Izarothy <izarothmail@gmail.com>
2023-07-14 18:16:25 +0200

    style: add montserrat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  display: "swap",
+  subsets: ["latin"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${montserrat.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
